1974cd5e4fca3951e5d6b40b828c9bc94cc42e9e
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-12-07 13:39:04 +0100

    Update about.dart

--- about.dart
+++ about.dart
@@ -21,16 +21,37 @@ class about extends StatelessWidget {
                       letterSpacing: 12,
                     ))),
             Container(
+              child: Icon(
+                Icons.contacts_rounded,
+                size: 100,
+              ),
+              // child: Text("Header",
+              //     style: TextStyle(
+              //       color: Colors.green,
+              //       fontSize: 30,
+              //       fontWeight: FontWeight.w100,
+              //       wordSpacing: 30,
+              //       letterSpacing: 12,
+              //     )),
               height: 200,
+              width: 300,
               decoration: BoxDecoration(
                   color: Colors.red,
                   border: Border.all(
                     color: Colors.black,
                     width: 3,
                   ),
-                  borderRadius: BorderRadius.circular(20)),
+                  borderRadius: BorderRadius.circular(0)),
             ),
             Container(
+              child: Text("Name",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w200,
+                    wordSpacing: 30,
+                    letterSpacing: 12,
+                  )),
               height: 100,
               width: MediaQuery.of(context).size.width,
               color: Colors.blue,
@@ -38,11 +59,27 @@ class about extends StatelessWidget {
             Row(
               children: [
                 Container(
+                  child: Text("Sex",
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w300,
+                        wordSpacing: 30,
+                        letterSpacing: 12,
+                      )),
                   height: 100,
                   width: MediaQuery.of(context).size.width / 2,
                   color: Colors.orange,
                 ),
                 Container(
+                  child: Text("Occupation",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w400,
+                        wordSpacing: 30,
+                        letterSpacing: 12,
+                      )),
                   height: 100,
                   width: MediaQuery.of(context).size.width / 2,
                   color: Colors.purple,
