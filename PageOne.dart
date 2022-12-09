048b955a29eda5970db7c9f9b8ac4f5720643637
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-12-09 11:32:26 +0100

    PageOne dart file

--- PageOne.dart
+++ PageOne.dart
@@ -6,3 +6,26 @@ class PageOne extends StatefulWidget {
   @override
   State<PageOne> createState() => _PageOneState();
 }
+
+class _PageOneState extends State<PageOne> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Page One"),
+        ),
+        body: Center(
+            child: Container(
+                height: 40,
+                color: Colors.red,
+                child: MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Go to Page Two",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )))));
+  }
+}
