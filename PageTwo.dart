d821c12bab832c7409214ea30c8aa16ac373c6a9
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-12-09 11:33:28 +0100

    Update PageTwo.dart

--- PageTwo.dart
+++ PageTwo.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 class PageTwo extends StatefulWidget {
   const PageTwo({super.key});
+
+    @override
+  State<PageTwo> createState() => _PageTwoState();
+}
