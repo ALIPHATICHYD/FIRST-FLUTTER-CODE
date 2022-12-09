c582c0b921be79d7d8e575b7b26187ac5b61bc75
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-12-09 11:39:02 +0100

    Adding PageThree dart
    Adding PageThree dart file that will Link to PageTwo

--- PageThree.dart
+++ PageThree.dart
@@ -6,3 +6,29 @@ class PageThree extends StatefulWidget {
   @override
   State<PageThree> createState() => _PageThreeState();
 }
+
+
+class _PageThreeState extends State<PageThree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Page Three"),
+        ),
+        body: Center(
+          child: Container(
+            height: 40,
+            color: Colors.red,
+            child: MaterialButton(
+              onPressed: () {},
+              child: const Text(
+                "Go to Page Two",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
