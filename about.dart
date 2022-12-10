a0bad701e0ff4aa1213b89e852b14ec714ccbe89
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-12-10 20:51:38 +0100

    Update about.dart

--- about.dart
+++ about.dart
@@ -109,8 +109,3 @@ import 'package:flutter/material.dart';
 class about extends StatelessWidget {
   const about({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome"),
